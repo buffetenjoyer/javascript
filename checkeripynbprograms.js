7888fe901c1801707d05537782d8e2e1b9141998
Henry Han <henryhasnotime@gmail.com>
2025-06-17 22:39:28 -0400

    update

--- checkeripynbprograms.js
+++ checkeripynbprograms.js
@@ -1,40 +1,76 @@
-//   Chapter 5 Excercise 3
+//   Chapter 6 Excercise 2
 
-function every(inputArray, predicateFunction) {
-    for (let input of inputArray) {
-        if (!predicateFunction(input)) {
-            return false;
+
+class Group {
+    constructor() {
+        this.parts = [];
+    }
+
+    add(x) {
+        if (!this.has(x)) {
+            this.parts.push(x);
         }
     }
-    return true;
-}
 
-function every2(inputArray, predicateFunction) {     
-    /*if (!inputArray.some(!predicateFunction())) {   // this would've worked if notated correctly, and the reason it would work is basically set theory: the "every" function is basically the "and" function for arrays
-                                                      // and the "some" function is basically the "or" function for arrays. From set theory, we learned that
-                                                      // !(A and B) = (!A or !B), and this is equivalent to (A and B) = !(!A or !B). We use the latter (second) equation in our program.
-                                                      // The expression !inputArray.some(!predicateFunction()) basically means !(!A or !B or ... or !N) which equals (A and B and ... and N)
-                                                      // and that should equal true if and only if the "every" function returns true, and equal false if and only if the "every" function returns false.
-                                                      // So we use if statement to make sure the function only returns true if (A and B and ... and N) equals true, and false for otherwise.
-        return true;                                    
-    }*/
-
-    if (!inputArray.some(i => !predicateFunction(i))) {   // same idea as the code that was commented out (follows same ideas in above comments too), but notated correctly this time
-                                                          // the reason why above code creates error is beacuse !predicateFunction() is not a function value, it's actually a boolean value, you can't just put an ! mark in front of a function to make another function/(function value). Maybe you can in other cases (I tested in code block below this one)
-        return true;
+    delete(x) {
+        if (this.has(x)){
+            this.parts = this.parts.slice(0,this.parts.indexOf(x)).concat(this.parts.slice(this.parts.indexOf(x)+1));
+        }
     }
-    return false;
+
+    has(x) {
+        if (this.parts.indexOf(x) == -1) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    static from(someIterable) {
+        let group = new Group()
+        for (let item of someIterable) {
+            group.add(item);            
+        }
+        return group;
+    }
+
 }
 
-console.log(every([1,2,3], i => i < 5));
-console.log(every2([1,2,3], i => i < 5));
+let underlying = [];
 
-console.log(every([1,2,3], i => i < 3));
-console.log(every2([1,2,3], i => i < 3));
+underlying.push(1);
+console.log(underlying);
+underlying.push(1);
+console.log(underlying);
+console.log(underlying.indexOf(1));
+console.log(underlying.indexOf(2));
 
-console.log(every(["hi", true, "yes", 17, x => {x < 22}], i => typeof(i) != "object"));
-console.log(every2(["hi", true, "yes", 17, x => {x < 22}], i => typeof(i) != "object"));
+underlying.push(5);
+console.log(underlying.slice(1));
+console.log(underlying.slice(0, 1));
+console.log(2 == "2");
+console.log("------------------");
 
-console.log(every(["hi", true, "yes", 17, x => {x < 22}], i => typeof(i) != "function"));
-console.log(every2(["hi", true, "yes", 17, x => {x < 22}], i => typeof(i) != "function"));
+
+let group1 = new Group();
+
+group1.add(1);
+group1.delete(2);
+group1.add(2);
+group1.add(3);
+group1.delete(2);
+group1.add(1);
+group1.add("1");
+group1.delete("1");
+console.log(group1.has(3));
+console.log(group1.has(2));
+console.log(group1.parts);
+
+
+let underlying2 = ["happy", "happy", "happie", "ding", 2, 6, 1, 2, "2", "5", "2"];
+
+let group2 = Group.from(underlying2);
+
+console.log(group2);
 
