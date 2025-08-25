5faedd4dbbcff8396d4b8f27afe587475aa48810
Henry Han <henryhasnotime@gmail.com>
2025-08-25 19:01:25 -0400

    update

---
+++ eloquentJS/ch21/ch21testing.mjs
@@ -0,0 +1,9 @@
+let regex = /a(b)c/g;
+let result = regex.exec("111abc2222abc");
+console.log(result);
+
+// let obj = {a: 1, b: 2};
+// console.log(obj);
+
+// let arr = [1,2,3,4];
+// console.log(arr.slice());
